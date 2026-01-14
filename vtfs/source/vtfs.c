2cc1270501708dc0e009e1b504c843ae3dae4811
palloHiiri <408322@niuitmo.ru>
2026-01-14 16:51:46 +0000

    pt6

--- vtfs/source/vtfs.c
+++ vtfs/source/vtfs.c
@@ -1,198 +1,187 @@
 #include "vtfs.h"
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
 
-#define MODULE_NAME "vtfs"
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("secs-dev");
-MODULE_DESCRIPTION("A simple FS kernel module");
+MODULE_DESCRIPTION("A simple FS kernel module with RAM storage");
 
-
-struct file_system_type vtfs_fs_type = {
-  .name = "vtfs",
-  .mount = vtfs_mount,
-  .kill_sb = vtfs_kill_sb,
-};
+LIST_HEAD(vtfs_files);
+int next_ino = 1001;
+DEFINE_MUTEX(vtfs_files_lock);
 
 struct inode_operations vtfs_inode_ops = {
-  .lookup = vtfs_lookup,
+    .lookup = vtfs_lookup,
+    .create = vtfs_create,
+    .unlink = vtfs_unlink,
 };
 
 struct file_operations vtfs_dir_ops = {
-  .iterate_shared = vtfs_iterate,
-};
-struct file_operations vtfs_file_ops = {
+    .iterate_shared = vtfs_iterate,
 };
 
-struct vtfs_file_info* find_file_info(const char* name, ino_t parent_ino) {
+struct file_operations vtfs_file_ops = {
+    
+};
+
+struct file_system_type vtfs_fs_type = {
+    .owner = THIS_MODULE,
+    .name = "vtfs",
+    .mount = vtfs_mount,
+    .kill_sb = vtfs_kill_sb,
+    .fs_flags = FS_USERNS_MOUNT,
+};
+
+static void init_root_directory(void) {
+    struct vtfs_file_info* root_dir = kmalloc(sizeof(*root_dir), GFP_KERNEL);
+    if(!root_dir) {
+        LOG("Failed to allocate memory for root directory\n");
+        return;
+    }
+
+    strcpy(root_dir->name, ".");
+    root_dir->ino = VTFS_ROOT_INO;
+    root_dir->parent_ino = VTFS_ROOT_INO;
+    root_dir->is_dir = true;
+    INIT_LIST_HEAD(&root_dir->list);
+    list_add(&root_dir->list, &vtfs_files);
+    LOG("Root directory initialized\n");
+}
+
+struct vtfs_file_info* find_file_info(ino_t ino) {
     struct vtfs_file_info *file_info;
     
     list_for_each_entry(file_info, &vtfs_files, list) {
-        if (strcmp(file_info->name, name) == 0 && file_info->exists) {
+        if(file_info->ino == ino) {
             return file_info;
         }
     }
     return NULL;
 }
 
-struct vtfs_file_info* create_file_info(const char* name, ino_t ino) {
-    struct vtfs_file_info *file_info = kmalloc(sizeof(*file_info), GFP_KERNEL);
-    if (!file_info) {
-        return NULL;
+struct vtfs_file_info* find_file_in_dir(const char* name, ino_t parent_ino) {
+    struct vtfs_file_info *file_info;
+    
+    list_for_each_entry(file_info, &vtfs_files, list) {
+        if(file_info->parent_ino == parent_ino && 
+           strcmp(file_info->name, name) == 0) {
+            return file_info;
+        }
     }
-    
-    strncpy(file_info->name, name, sizeof(file_info->name) - 1);
-    file_info->name[sizeof(file_info->name) - 1] = '\0';
-    file_info->ino = ino;
-    file_info->exists = true;
-    
-    INIT_LIST_HEAD(&file_info->list);
-    list_add(&file_info->list, &vtfs_files);
-    
-    return file_info;
+    return NULL;
 }
 
 struct dentry* vtfs_lookup(struct inode* parent_inode, 
                           struct dentry* child_dentry, 
-                          unsigned int flag){
-  ino_t root = parent_inode->i_ino;
-  const char* name = child_dentry->d_name.name;
-  if(root == 1000 && !strcmp(name, "test.txt")) {
-    struct inode* inode = vtfs_get_inode(
-      parent_inode->i_sb, 
-      parent_inode, 
-      S_IFREG | 0777, 
-      101
-    );
-    if(inode) {
-      inode->i_fop = &vtfs_file_ops;
-      d_add(child_dentry, inode);
-      LOG("Created file inode for %s\n", name);
-    }
-  }else if (!strcmp(name, "dir"))
-  {
-    struct inode* inode = vtfs_get_inode(
-      parent_inode->i_sb, 
-      parent_inode, 
-      S_IFDIR | 0777, 
-      200
-    );
-    if(inode) {
-      inode->i_op = &vtfs_inode_ops;
-      inode->i_fop = &vtfs_dir_ops;
-      d_add(child_dentry, inode);
-      LOG("Created directory inode for %s\n", name);
-    }
-  }
+                          unsigned int flag) {
+    const char* name = child_dentry->d_name.name;
+    struct vtfs_file_info* file_info;
 
-  return NULL;
+    file_info = find_file_in_dir(name, parent_inode->i_ino);
+    if(file_info) {
+        struct inode* inode = vtfs_get_inode(
+            parent_inode->i_sb,
+            NULL,
+            file_info->is_dir ? S_IFDIR | 0777 : S_IFREG | 0777,
+            file_info->ino
+        );
+        if(inode) {
+            inode->i_fop = file_info->is_dir ? &vtfs_dir_ops : &vtfs_file_ops;
+            inode->i_op = &vtfs_inode_ops;
+            d_add(child_dentry, inode);
+            LOG("Lookup successful for '%s' in directory %lu\n", 
+                name, parent_inode->i_ino);
+        } else {
+            LOG("Failed to create inode during lookup for '%s'\n", name);
+        }
+    } else {
+        LOG("Lookup failed for '%s' in directory %lu\n", name, parent_inode->i_ino);
+    }
+    return NULL;
 }
 
 int vtfs_create(struct mnt_idmap *idmap, struct inode *parent_inode,
                 struct dentry *child_dentry, umode_t mode, bool b) {
-    ino_t parent_ino = parent_inode->i_ino;
     const char *name = child_dentry->d_name.name;
     
     LOG("create called for: '%s' in directory %lu, mode=%o\n", 
-        name, parent_ino, mode);
+        name, parent_inode->i_ino, mode);
+
+    mutex_lock(&vtfs_files_lock);
     
-    if (parent_ino == 1000) {
-        mutex_lock(&vtfs_files_lock);
-        
-        if (find_file_info(name, parent_ino)) {
-            mutex_unlock(&vtfs_files_lock);
-            LOG("File '%s' already exists\n", name);
-            return -EEXIST;
-        }
-        
-        int new_ino = next_ino++;
-        struct inode *inode = vtfs_get_inode(
-            parent_inode->i_sb, 
-            parent_inode, 
-            mode | 0777, 
-            new_ino
-        );
-        
-        if (!inode) {
-            mutex_unlock(&vtfs_files_lock);
-            LOG("Failed to create inode for '%s'\n", name);
-            return -ENOMEM;
-        }
-        
-        inode->i_fop = &vtfs_file_ops;
-        
-        if (!create_file_info(name, new_ino)) {
-            iput(inode);
-            mutex_unlock(&vtfs_files_lock);
-            LOG("Failed to save file info for '%s'\n", name);
-            return -ENOMEM;
-        }
-        
-        if (!strcmp(name, "test.txt")) {
-            file_mask |= 1;
-        } else if (!strcmp(name, "new_file.txt")) {
-            file_mask |= 2;
-        }
-        
-        d_add(child_dentry, inode);
-        
+    if(find_file_in_dir(name, parent_inode->i_ino)) {
         mutex_unlock(&vtfs_files_lock);
-        
-        LOG("Created file '%s' with ino=%d, mask=0x%x\n", 
-            name, new_ino, file_mask);
-        return 0;
+        LOG("File '%s' already exists\n", name);
+        return -EEXIST;
     }
-    
-    LOG("Can only create files in root directory\n");
-    return -EPERM;
+
+    struct vtfs_file_info *new_file_info = kmalloc(sizeof(*new_file_info), GFP_KERNEL);
+    if (!new_file_info) {
+        mutex_unlock(&vtfs_files_lock);
+        LOG("Memory allocation failed for new file '%s'\n", name);
+        return -ENOMEM;
+    }
+
+    strncpy(new_file_info->name, name, sizeof(new_file_info->name)-1);
+    new_file_info->name[sizeof(new_file_info->name)-1] = '\0';
+    new_file_info->ino = next_ino++;
+    new_file_info->parent_ino = parent_inode->i_ino;
+    new_file_info->is_dir = false;
+    INIT_LIST_HEAD(&new_file_info->list);
+    list_add(&new_file_info->list, &vtfs_files);
+
+    struct inode* inode = vtfs_get_inode(
+        parent_inode->i_sb,
+        NULL,
+        S_IFREG | 0777,
+        new_file_info->ino
+    );
+    if (!inode) {
+        list_del(&new_file_info->list);
+        kfree(new_file_info);
+        mutex_unlock(&vtfs_files_lock);
+        LOG("Failed to create inode for new file '%s'\n", name);
+        return -ENOMEM;
+    }
+    inode->i_fop = &vtfs_file_ops;
+    inode->i_op = &vtfs_inode_ops;
+    d_add(child_dentry, inode);
+    LOG("Created file '%s' with inode %lu\n", name, new_file_info->ino);
+    mutex_unlock(&vtfs_files_lock);
+    return 0;
 }
 
 int vtfs_unlink(struct inode *parent_inode, struct dentry *child_dentry) {
     const char *name = child_dentry->d_name.name;
-    ino_t parent_ino = parent_inode->i_ino;
     
-    LOG("unlink called for: '%s' in directory %lu\n", name, parent_ino);
+    LOG("unlink called for: '%s' in directory %lu\n", 
+        name, parent_inode->i_ino);
     
-    if (parent_ino == 1000) {
-        mutex_lock(&vtfs_files_lock);
-        
-        struct vtfs_file_info *file_info = find_file_info(name, parent_ino);
-        if (file_info) {
-            file_info->exists = false;
-            
-            if (!strcmp(name, "test.txt")) {
-                file_mask &= ~1;
-            } else if (!strcmp(name, "new_file.txt")) {
-                file_mask &= ~2;
-            }
-            
-            LOG("Unlinked file '%s', mask=0x%x\n", name, file_mask);
-            mutex_unlock(&vtfs_files_lock);
-            return 0;
-        }
-        
+    mutex_lock(&vtfs_files_lock);
+    struct vtfs_file_info *file_info = find_file_in_dir(name, parent_inode->i_ino);
+    if (file_info) {
+        list_del(&file_info->list);
+        kfree(file_info);
         mutex_unlock(&vtfs_files_lock);
-        LOG("File '%s' not found\n", name);
-        return -ENOENT;
+        LOG("Unlinked file '%s'\n", name);
+        return 0;
     }
-    
-    return -EPERM;
+    mutex_unlock(&vtfs_files_lock);
+    LOG("File '%s' not found for unlinking\n", name);
+    return -ENOENT;
 }
 
 struct dentry* vtfs_mount(struct file_system_type *fs_type,
                          int flags, const char *token,
                          void *data) {
-  struct dentry *ret = mount_nodev(fs_type, flags, data, vtfs_fill_super);
-  if(ret == NULL) {
-    printk(KERN_ERR "Can't mount file system");
-  } else {
-    printk(KERN_INFO "Mounted successfuly");
-  }
-  return ret;
-  
+    struct dentry *ret = mount_nodev(fs_type, flags, data, vtfs_fill_super);
+    if(ret == NULL) {
+        LOG("Can't mount file system\n");
+    } else {
+        LOG("Mounted successfully with token: %s\n", token);
+    }
+    return ret;
 }
 
 int vtfs_iterate(struct file* filp, struct dir_context* ctx) {
@@ -200,94 +189,123 @@ int vtfs_iterate(struct file* filp, struct dir_context* ctx) {
     struct inode* inode   = dentry->d_inode;
     ino_t ino             = inode->i_ino;
 
-    if (ino == 1000) {
-        if (ctx->pos == 0) {
-            if (!dir_emit(ctx, ".", 1, ino, DT_DIR))
-                return 0;
-            ctx->pos++;
+    if(ctx->pos < 0){
+        return 0;
+    }
+
+    if(ctx->pos == 0) {
+        if(!dir_emit(ctx, ".", 1, ino, DT_DIR)) {
+            return 0;
         }
+        ctx->pos++;
+    }
+
+    if(ctx->pos == 1) {
+        struct vtfs_file_info* current_dir = find_file_info(ino);
+        ino_t parent_ino = current_dir ? current_dir->parent_ino : VTFS_ROOT_INO;
         
-        if (ctx->pos == 1) {
-            ino_t parent_ino = dentry->d_parent ? dentry->d_parent->d_inode->i_ino : ino;
-            if (!dir_emit(ctx, "..", 2, parent_ino, DT_DIR))
-                return 0;
-            ctx->pos++;
+        if(!dir_emit(ctx, "..", 2, parent_ino, DT_DIR)) {
+            return 0;
         }
-        
-        if (ctx->pos == 2) {
-            if (!dir_emit(ctx, "test.txt", 8, 101, DT_REG))
-                return 0;
-            ctx->pos++;
-        }
+        ctx->pos++;
     }
     
+    struct vtfs_file_info* file_info;
+    int count = 2;
+
+    list_for_each_entry(file_info, &vtfs_files, list) {
+        if(file_info->parent_ino == ino) {
+            if(count == ctx->pos) {
+                unsigned char type = file_info->is_dir ? DT_DIR : DT_REG;
+                if(!dir_emit(ctx, file_info->name, strlen(file_info->name), 
+                           file_info->ino, type)) {
+                    return 0;
+                }
+                ctx->pos++;
+                return 0;
+            }
+            count++;
+        }
+    }
     return 0;
 }
 
-
-
-struct dentry* mount_nodev(
-  struct file_system_type* fs_type,
-  int flags, 
-  void* data, 
-  int (*fill_super)(struct super_block*, void*, int)
-);
-
 int vtfs_fill_super(struct super_block *sb, void *data, int silent) {
-  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR|0777, 1000);
+    struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR|0777, VTFS_ROOT_INO);
 
-  if (inode == NULL) {
-    printk(KERN_ERR "Can't create root inode");
-    return -ENOMEM;
-  }
-  
-  inode->i_op = &vtfs_inode_ops;
-  inode->i_fop = &vtfs_dir_ops;
+    if (inode == NULL) {
+        LOG("Can't create root inode\n");
+        return -ENOMEM;
+    }
+    
+    inode->i_op = &vtfs_inode_ops;
+    inode->i_fop = &vtfs_dir_ops;
 
-  sb->s_root = d_make_root(inode);
-  if (sb->s_root == NULL) {
-    printk(KERN_ERR "Can't create root directory");
-    return -ENOMEM;
-  }
-  printk(KERN_INFO "Root directory created successfully");
-  return 0;
+    sb->s_root = d_make_root(inode);
+    if (sb->s_root == NULL) {
+        iput(inode);
+        LOG("Can't create root directory\n");
+        return -ENOMEM;
+    }
+    LOG("Root directory created successfully\n");
+    return 0;
 }
 
 struct inode* vtfs_get_inode(
-  struct super_block* sb, 
-  const struct inode* dir, 
-  umode_t mode, 
-  int i_ino
+    struct super_block* sb, 
+    const struct inode* dir, 
+    umode_t mode, 
+    int i_ino
 ) {
-  struct inode *inode = new_inode(sb);
-  if (inode != NULL) {
+    struct inode *inode = new_inode(sb);
+    if (inode != NULL) {
+        inode->i_mode = mode | 0777;
+        i_uid_write(inode, 0);
+        i_gid_write(inode, 0);
+        inode->i_ino = i_ino;
+        inode->__i_atime = inode->__i_mtime = inode->__i_ctime = current_time(inode);
 
-    inode_init_owner(&nop_mnt_idmap, inode, dir, mode);
-    inode->i_ino = i_ino;
-    inode->__i_atime = inode->__i_mtime = inode->__i_ctime = current_time(inode);
-
-    if(S_ISDIR(mode)) {
-      inode->i_fop = &vtfs_dir_ops;
-      inode->i_op = &vtfs_inode_ops;
+        if(S_ISDIR(mode)) {
+            inode->i_fop = &vtfs_dir_ops;
+            inode->i_op = &vtfs_inode_ops;
+        } else {
+            inode->i_fop = &vtfs_file_ops;
+            inode->i_op = &vtfs_inode_ops;
+        }
     }
-  }
 
-  return inode;
+    return inode;
 }
 
 void vtfs_kill_sb(struct super_block* sb) {
-  printk(KERN_INFO "vtfs super block is destroyed. Unmount successfully.\n");
+    struct vtfs_file_info* file_info, *tmp;
+    LOG("Unmounting VTFS filesystem\n");
+    list_for_each_entry_safe(file_info, tmp, &vtfs_files, list) {
+        list_del(&file_info->list);
+        kfree(file_info);
+    }
+    kill_litter_super(sb);
+    LOG("VTFS filesystem unmounted\n");
 }
 
 static int __init vtfs_init(void) {
-  register_filesystem(&vtfs_fs_type);
-  LOG("VTFS joined the kernel\n");
-  return 0;
+    int ret = register_filesystem(&vtfs_fs_type);
+    if (ret == 0) {
+        LOG("VTFS registered successfully\n");
+        init_root_directory();
+    } else {
+        LOG("Failed to register filesystem: %d\n", ret);
+    }
+    return ret;
 }
 
 static void __exit vtfs_exit(void) {
-  unregister_filesystem(&vtfs_fs_type);
-  LOG("VTFS left the kernel\n");
+    int ret = unregister_filesystem(&vtfs_fs_type);
+    if (ret == 0) {
+        LOG("VTFS unregistered successfully\n");
+    } else {
+        LOG("Failed to unregister filesystem: %d\n", ret);
+    }
 }
 
 module_init(vtfs_init);
--- vtfs/source/vtfs.h
+++ vtfs/source/vtfs.h
@@ -3,36 +3,48 @@
 
 #include <linux/inet.h>
 
+#define MODULE_NAME "vtfs"
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
+#define VTFS_ROOT_INO 1000
 
-static int file_mask = 0;
+struct vtfs_file_content {
+    char *data;
+    size_t size;
+    size_t allocated;
+};
 
 struct vtfs_file_info {
     char name[256];
     ino_t ino;
-    bool exists;
+    ino_t parent_ino;
+    bool is_dir;
     struct list_head list;
+    struct vtfs_file_content content;
+    struct mutex lock;
 };
 
-static LIST_HEAD(vtfs_files);
-static DEFINE_MUTEX(vtfs_files_lock);
-static int next_ino = 103;
+extern struct list_head vtfs_files;
+extern int next_ino;
+extern struct mutex vtfs_files_lock;
+extern int file_mask;
 
-struct dentry* vtfs_mount(struct file_system_type *fs_type,
-                         int flags, const char *token,
-                         void *data);
-void vtfs_kill_sb(struct super_block *sb);
+struct dentry* vtfs_mount(struct file_system_type* fs_type, int flags, const char* token, void* data);
+void vtfs_kill_sb(struct super_block* sb);
 int vtfs_fill_super(struct super_block *sb, void *data, int silent);
-struct inode* vtfs_get_inode(struct super_block *sb, const struct inode *inode,
-              umode_t mode, int i_ino);
-struct dentry* vtfs_lookup(struct inode* parent_inode, 
-                          struct dentry* child_dentry, 
-                          unsigned int flag);
+struct inode* vtfs_get_inode(struct super_block* sb, const struct inode* dir, umode_t mode, int i_ino);
+struct dentry* vtfs_lookup(struct inode* parent_inode, struct dentry* child_dentry, unsigned int flag);
 int vtfs_iterate(struct file* filp, struct dir_context* ctx);
-int vtfs_create(struct mnt_idmap *idmap, struct inode *parent_inode,
-                struct dentry *child_dentry, umode_t mode, bool b);
+int vtfs_create(struct mnt_idmap *idmap, struct inode *parent_inode, struct dentry *child_dentry, umode_t mode, bool b);
 int vtfs_unlink(struct inode *parent_inode, struct dentry *child_dentry);
-struct vtfs_file_info* find_file_info(const char* name, ino_t parent_ino);
-struct vtfs_file_info* create_file_info(const char* name, ino_t ino);
+ssize_t vtfs_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset);
+ssize_t vtfs_write(struct file *filp, const char __user *buffer, size_t length, loff_t *offset);
+
+struct vtfs_file_info *find_file_info(ino_t ino);
+struct vtfs_file_info *find_file_in_dir(const char *name, ino_t parent_ino);
+
+extern struct inode_operations vtfs_inode_ops;
+extern struct file_operations vtfs_dir_ops;
+extern struct file_operations vtfs_file_ops;
+extern struct file_system_type vtfs_fs_type;
 
 #endif // VTFS_H
